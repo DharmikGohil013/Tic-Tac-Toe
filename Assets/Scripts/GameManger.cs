b95d64234dc81fcc133fcfe8c1c97045890f3ff9
DharmikGohil013 <dharmikgohil395003@gmail.com>
2024-09-19 10:16:31 +0530

    add more

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class GameManger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManger Instance;
+    private void Awake()
     {
-        
-    }
+        if(Instance != null && Instance!=false)
+        {
+            Destroy(gameObject);
 
-    // Update is called once per frame
-    void Update()
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    private void Start()
     {
-        
+        NetworkManager.Singleton.OnClientConnectedCallback += (clientId) =>
+        {
+            Debug.Log(clientId + "joined");
+        };
+    }
+    public void startHst()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+    public void StartClint()
+    {
+        NetworkManager.Singleton.StartClient();
     }
 }
